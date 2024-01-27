25fa447695d7fcd2de5f44d5f90e0dd7e577597f
Alwazir Fitrah <alwazirf@gmail.com>
2024-01-28 01:04:07 +0700

    added hero section and stats section

--- src/components/Bussiness.jsx
+++ src/components/Bussiness.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { features } from "../constants";
+import styles, { layout } from "../styles";
+import Button from "./Button";
 
-const Bussiness = () => {
-  return <div>Bussiness</div>;
-};
+const Bussiness = () => (
+  <section id="features" className={layout.section}>
+    wait
+  </section>
+);
 
 export default Bussiness;
