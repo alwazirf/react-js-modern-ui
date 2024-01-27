ba351a929d602c8bf3748b3a15a20794718ffd7b
Alwazir Fitrah <alwazirf@gmail.com>
2024-01-27 13:22:57 +0700

    create navbar and hero section

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import styles from "./styles";
+
+import {
+  Navbar,
+  Hero,
+  Billing,
+  Bussiness,
+  CTA,
+  CardDeal,
+  Client,
+  Footer,
+  Stats,
+  Testimonial,
+} from "./components";
+
+const App = () => (
+  <div>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Bussiness />
+          <Billing />
+          <CardDeal />
+          <Testimonial />
+          <Client />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Bussiness from "./Bussiness";
+import Client from "./Client";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonial from "./Testimonial";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Bussiness,
+  Client,
+  CTA,
+  Stats,
+  Footer,
+  Testimonial,
+  Hero,
+};
